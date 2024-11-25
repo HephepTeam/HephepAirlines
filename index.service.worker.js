cc3bbdaabaf8e5df77f7116e8df5270cead7ea2c
Hephep <alkounet95@gmail.com>
2024-11-25 13:00:48 +0100

    traduction

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1732534645|12949003236';
+const CACHE_VERSION = '1732536023|14327633226';
 /** @type {string} */
 const CACHE_PREFIX = 'Hephep Airlines-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
