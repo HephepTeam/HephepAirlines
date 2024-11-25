cda808a80429b657ed54a8195ca698372881f49a
Hephep <alkounet95@gmail.com>
2024-11-25 11:29:43 +0100

    update to resize background and window size

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1732529965|8269138301';
+const CACHE_VERSION = '1732530519|8823315005';
 /** @type {string} */
 const CACHE_PREFIX = 'Hephep Airlines-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
