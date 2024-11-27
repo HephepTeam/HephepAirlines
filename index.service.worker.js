923f7cee570ccb78a4c3d882fa5f72b0f51dbc3a
Hephep <alkounet95@gmail.com>
2024-11-27 08:46:13 +0100

    added music

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1732556340|34644005376';
+const CACHE_VERSION = '1732693441|214557123';
 /** @type {string} */
 const CACHE_PREFIX = 'Hephep Airlines-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
